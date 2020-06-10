7d61224c2d7b5a0fc395a28b818db7451f75706f
Samet İrkören <sametirkoren@gmail.com>
2020-06-10 17:51:24 +0300

    Fluent Api AppUserRole and Product

---
+++ JwtProject.DataAccess/Concrete/EntityFrameworkCore/Mapping/AppUserRoleMap.cs
@@ -0,0 +1,20 @@
+﻿using JwtProject.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtProject.DataAccess.Concrete.EntityFrameworkCore.Mapping
+{
+    public class AppUserRoleMap : IEntityTypeConfiguration<AppUserRole>
+    {
+        public void Configure(EntityTypeBuilder<AppUserRole> builder)
+        {
+            builder.HasKey(I => I.Id);
+            builder.Property(I => I.Id).UseIdentityColumn();
+
+            builder.HasIndex(I => new { I.AppRoleId, I.AppUserId }).IsUnique();
+        }
+    }
+}
---
+++ JwtProject.DataAccess/Concrete/EntityFrameworkCore/Mapping/ProductMap.cs
@@ -0,0 +1,20 @@
+﻿using JwtProject.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtProject.DataAccess.Concrete.EntityFrameworkCore.Mapping
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(I => I.Id);
+            builder.Property(I => I.Id).UseIdentityColumn();
+
+            builder.Property(I => I.Name).HasMaxLength(150).IsRequired();
+        }
+    }
+}
