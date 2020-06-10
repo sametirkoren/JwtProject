a5d8f67bf95d477085d16235c729ee0a98023795
Samet İrkören <sametirkoren@gmail.com>
2020-06-10 20:13:36 +0300

    Business GenericManager created

---
+++ Jwt.Business/Concrete/GenericManager.cs
@@ -0,0 +1,43 @@
+﻿using Jwt.Business.Interfaces;
+using JwtProject.DataAccess.Interfaces;
+using JwtProject.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jwt.Business.Concrete
+{
+    public class GenericManager<TEntity> : IGenericService<TEntity> where TEntity : class, ITable, new()
+    {
+        private readonly IGenericDal<TEntity> _genericDal;
+        public GenericManager(IGenericDal<TEntity> genericDal)
+        {
+            _genericDal = genericDal;
+        }
+        public async Task Add(TEntity entity)
+        {
+            await _genericDal.Add(entity);
+        }
+
+        public async Task<List<TEntity>> GetAll()
+        {
+            return await _genericDal.GetAll();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _genericDal.GetById(id);
+        }
+
+        public async Task Remove(TEntity entity)
+        {
+            await _genericDal.Remove(entity);
+        }
+
+        public async Task Update(TEntity entity)
+        {
+            await _genericDal.Update(entity);
+        }
+    }
+}
